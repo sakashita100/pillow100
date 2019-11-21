ef2a1bfb8ffc9e9d6f7d3ece32afece7a97cd3fa
Miwa Mayama <b1602995@planet.kanazawa-it.ac.jp>
2019-11-21 19:23:54 +0900

    add test

--- heartbeat.js
+++ heartbeat.js
@@ -1,48 +1,22 @@
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
+window.AudioContext = window.AudioContext || window.webkitAudioContext;  
 var context = new AudioContext();
-var soundBuffer;
 
-var request = new XMLHttpRequest();
-request.open('GET', './sound/heart.mp3', true);
-request.responseType = 'arraybuffer';
+var getAudioBuffer = function(url, fn) {  
+  var request = new XMLHttpRequest();
+  request.responseType = 'arraybuffer';
 
-// Decode asynchronously
-request.onload = function() {
-    context.decodeAudioData(request.response, function(buffer) {
-        soundBuffer = buffer;
-    });
-}
-request.send();
-
-      
-var RhythmSample = {};
-RhythmSample.play = function() {
-    function playSound(buffer, time) {
-        var source = context.createBufferSource();
-        source.buffer = buffer;
-        source.connect(context.destination);
-        if (!source.start)
-            source.start = source.noteOn;
-            source.start(time);
+  request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+      if (request.status === 0 || request.status === 200) {
+        context.decodeAudioData(request.response, function(buffer) {
+          fn(buffer);
+        });
+      }
     }
+  };
 
-    var beat = buffer;
-
-    // We'll start playing the rhythm 100 milliseconds from "now"
-    var startTime = context.currentTime + 0.100;
-    var tempo = 80; // BPM (beats per minute)
-    var eighthNoteTime = (60 / tempo) / 2;
-
-    // Play 2 bars of the following:
-    for (var bar = 0; bar < 2; bar++) {
-        var time = startTime + bar * 8 * eighthNoteTime;
-        // Play the bass (kick) drum on beats 1, 5
-        playSound(beat, time);
-        playSound(beat, time + 2 * eighthNoteTime);
-        playSound(beat, time + 4 * eighthNoteTime);
-        playSound(beat, time + 6 * eighthNoteTime);
-
-    }
+  request.open('GET', url, true);
+  request.send('');
 };
 
 function playSound(buffer, time) {
@@ -50,4 +24,28 @@ function playSound(buffer, time) {
     source.buffer = buffer;
     source.connect(context.destination);
     source.start(time);
+}
+
+var beat = null;
+
+//main
+window.onload = function() {
+    getAudioBuffer('./sound/heart.mp3', function(buffer) {
+        beat = buffer;
+    });
+};
+
+var start = function() {
+    // We'll start playing the rhythm 100 milliseconds from "now"
+    var startTime = context.currentTime + 0.100;
+    var tempo = 80; // BPM (beats per minute)
+    var eighthNoteTime = (60 / tempo) / 2;
+
+    for (var bar = 0; bar < 2; bar++) {
+        var time = startTime + bar * 8 * eighthNoteTime;
+        playSound(beat, time);
+        playSound(beat, time + 2 * eighthNoteTime);
+        playSound(beat, time + 4 * eighthNoteTime);
+        playSound(beat, time + 6 * eighthNoteTime);
+    }
 }
