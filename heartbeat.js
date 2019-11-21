c7fa8ff81add9bbb9beb889fe4909a60c15be5f2
Miwa Mayama <b1602995@planet.kanazawa-it.ac.jp>
2019-11-21 19:07:29 +0900

    add

--- heartbeat.js
+++ heartbeat.js
@@ -24,30 +24,30 @@ RhythmSample.play = function() {
         if (!source.start)
             source.start = source.noteOn;
             source.start(time);
-        }
+    }
 
-        var beat = buffer;
+    var beat = buffer;
 
-        // We'll start playing the rhythm 100 milliseconds from "now"
-        var startTime = context.currentTime + 0.100;
-        var tempo = 80; // BPM (beats per minute)
-        var eighthNoteTime = (60 / tempo) / 2;
+    // We'll start playing the rhythm 100 milliseconds from "now"
+    var startTime = context.currentTime + 0.100;
+    var tempo = 80; // BPM (beats per minute)
+    var eighthNoteTime = (60 / tempo) / 2;
 
-        // Play 2 bars of the following:
-        for (var bar = 0; bar < 2; bar++) {
-          var time = startTime + bar * 8 * eighthNoteTime;
-          // Play the bass (kick) drum on beats 1, 5
-          playSound(beat, time);
-          playSound(beat, time + 2 * eighthNoteTime);
-          playSound(beat, time + 4 * eighthNoteTime);
-          playSound(beat, time + 6 * eighthNoteTime);
+    // Play 2 bars of the following:
+    for (var bar = 0; bar < 2; bar++) {
+        var time = startTime + bar * 8 * eighthNoteTime;
+        // Play the bass (kick) drum on beats 1, 5
+        playSound(beat, time);
+        playSound(beat, time + 2 * eighthNoteTime);
+        playSound(beat, time + 4 * eighthNoteTime);
+        playSound(beat, time + 6 * eighthNoteTime);
 
-        }
-      };
+    }
+};
 
-      function playSound(buffer, time) {
-        var source = context.createBufferSource();
-        source.buffer = buffer;
-        source.connect(context.destination);
-        source.start(time);
-      }
+function playSound(buffer, time) {
+    var source = context.createBufferSource();
+    source.buffer = buffer;
+    source.connect(context.destination);
+    source.start(time);
+}
