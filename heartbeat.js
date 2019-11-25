3163b1fc5ef765aacc68e3653f1b3429995e2569
Miwa Mayama <b1602995@planet.kanazawa-it.ac.jp>
2019-11-25 20:23:06 +0900

    add

--- heartbeat.js
+++ heartbeat.js
@@ -20,36 +20,59 @@ var getAudioBuffer = function(url, fn) {
 };
 
 function playSound(buffer, time) {
-    var source = context.createBufferSource();
-    source.buffer = buffer;
-    source.connect(context.destination);
-    source.start(time);
+  var source = context.createBufferSource();
+  source.buffer = buffer;
+  source.connect(context.destination);
+  source.start(time);
 }
 
 var beat = null;
 
 //main
 window.onload = function() {
-    getAudioBuffer('./sound/heart.mp3', function(buffer) {
-        beat = buffer;
-    });
+  getAudioBuffer('./sound/heart.mp3', function(buffer) {
+    beat = buffer;
+  });
 };
 
 var start = function() {
-    // We'll start playing the rhythm 100 milliseconds from "now"
-    var startTime = context.currentTime + 0.100;
-    var bpm = document.getElementById("bpm").value; // BPM (beats per minute)
-    var eighthNoteTime = (60 / bpm) / 2;
+  /*
+  const bpms = [
+    164,164,164,164,161,160,160,160,159,159,158,158,156,156,156,157,157,157,157,157,
+    157,157,157,157,157,157,158,158,157,156,156,156,157,157,158,158,158,158,165,167,
+    167,167,166,165,164,164,163,163,163,163,163,163,164,164,164,163,163,163,163,162,
+    162,162,162,162,162,163,163,165,166,166,166,166,166,166,167,170,170,170,181,181,
+    184,184,184,185,186,186,186,185,185,185,185,185,186,186,186,186,186,186,186,186,
+    186,186,186,186,186,186,186,188,188,188,188,188,188,188,188,188,188,188,188,188,
+    188,186,186,186,186,186,186,186,186,186,86,184,184,182,182,182,182,182,182,182,
+    181,181,180,180,180,180,180,180,180,180,180,180,175,175,175,169,166,166,166,166,
+    166,166,166,166,166,166,166,179,181,181,181,181,181,181,181,181,181,181,181,181,
+    181,182,182,182,182,182,182,183,183,183,186,186,186,186,188,188,188,188,188,188,
+    188,188,188,188,188,188,188,188,188,188,188,190,190,190,190,190,189,189,189,189,
+    189,189,189,188,185,185,185,185,185,185,185,185,185,185,185,185,185,185,185,185,
+    186,186,186,186,186,186,185,185,185,185,185,185,185,185,185,185
+  ];*/
 
-    for (var bar = 0; bar < 2; bar++) {
-        var time = startTime + bar * 8 * eighthNoteTime;
-        playSound(beat, time);
-        playSound(beat, time + 2 * eighthNoteTime);
-        playSound(beat, time + 4 * eighthNoteTime);
-        playSound(beat, time + 6 * eighthNoteTime);
-    }
-}
+  var startTime = context.currentTime + 0.100;
+  var bpm = document.getElementById("bpm").value; // BPM (beats per minute)
+  //var bpm = bpms[0];
+  //var eighthNoteTime = (60 / bpm) / 2;
+  var bps = 60 / bpm;
 
-var stop = function() {
+  /*
+  for (var cnt = 0; cnt < 2; cnt++) {
+    //bpm = bpms[cnt];
+    //document.getElementById("bpm").innerHTML = bpm;
+    //var time = startTime + cnt * 8 * eighthNoteTime;
+    var time = startTime + cnt 
+    playSound(beat, time);
+    playSound(beat, time + 2 * eighthNoteTime);
+    playSound(beat, time + 4 * eighthNoteTime);
+    playSound(beat, time + 6 * eighthNoteTime);
+  }*/
+
+  for (var i = 0; i < 8; ++i) {
+    playSound(beat, time + i * bps);
+  }
 
 }
