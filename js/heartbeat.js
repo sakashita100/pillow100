97dfedd684dbf62622c5809a965537fd58033d89
Miwa Mayama <b1602995@planet.kanazawa-it.ac.jp>
2019-11-27 16:48:57 +0900

    add

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -3,36 +3,31 @@ var context = null;
 var unlocked = false;
 var isPlaying = false;  //現在再生中かどうか
 var startTime;
-var current16thNote;  //現在の最後に予定されているメモは何か
+var currentNote;  //現在の最後に予定されているメモは何か
 var bpm = 60;
 var lookahead = 25.0; //スケジューリング関数を呼び出す間隔(ミリ秒)
 var scheduleAheadTime = 0.1;  //音をスケジュールする先読み時間の長さ(秒)
                               //先読みから計算され、タイマーが遅れた場合は次の間隔と重複する
 var nextNoteTime = 0.0; //次のメモの期限が来たとき
-var noteResolution = 0; //0 == 16分、1 == 8分、2 ==四分音符
+//var noteResolution = 2; //0 == 16分、1 == 8分、2 ==四分音符
 var noteLength = 0.05;  //ビープ音の長さ(秒単位)
-var beat = null;
+var beat = null; //心拍音を入れる箱
 
 
 function nextNote() {
-  //現在の音符と時間を次の16分音符に進める
-  //nextNoteTime変数とcurrent16thNote変数の更新
+  //現在の音符と時間を次の4分音符に進める
+  //nextNoteTime変数とcurrentNote変数の更新
   var bps = 60 / bpm;
-  nextNoteTime += 0.25 * bps;  //最後のビート時間に16分音符の長さのビートを追加する　16分音符 = 0.25 8分音符 = 0.5 4分音符 = bps
-  current16thNote++;  //ビート番号を進めてゼロに折り返す
-  if (current16thNote == 16) {
-      current16thNote = 0;
+  nextNoteTime += bps;  //最後のビート時間に16分音符の長さのビートを追加する　16分音符 = 0.25 8分音符 = 0.5 をbpsとかける
+  currentNote++;  //ビート番号を進めてゼロに折り返す
+  if (currentNote == 4) {
+      currentNote = 0;
   }
 }
 
 function scheduleNote( beatNumber, time ) {
   //次に鳴らすべきWebAudioの音をスケジューリングする
 
-  if ( (noteResolution==1) && (beatNumber%2))
-      return; //16分音符以外の8分音符は演奏しない
-  if ( (noteResolution==2) && (beatNumber%4))
-      return; //4分音符以外の8分音符を演奏しない
-  
   //AudioBufferSourceノードを作成して任意の音をここで設定できる
   var source = context.createBufferSource();
   source.buffer = beat;
@@ -46,7 +41,7 @@ function scheduler() {
   //存在したらWebAudioAPIを使って次の間隔の前に再生するノートをスケジュールし、ポインターを進める
   //この関数はlookaheadで設定したミリ秒ごとに呼ばれる
   while (nextNoteTime < context.currentTime + scheduleAheadTime ) {
-      scheduleNote( current16thNote, nextNoteTime );
+      scheduleNote( currentNote, nextNoteTime );
       nextNote();
   }
 }
@@ -64,7 +59,7 @@ function play() {
   isPlaying = !isPlaying;
 
   if (isPlaying) { // start playing
-      current16thNote = 0;
+      currentNote = 0;
       nextNoteTime = context.currentTime;
       timerWorker.postMessage("start");
       return "stop";
@@ -95,7 +90,7 @@ var getAudioBuffer = function(url, fn) {
 function init(){
   context = new AudioContext();
 
-  //オーディオファイルなどをロードする場合は、ここで行う
+  //オーディオファイルをロード
   getAudioBuffer('./sound/heart.mp3', function(buffer) {
     beat = buffer;
   });
