30c98c331cd5501eb03ec50a806ad2c10829e3b5
aa-adachi <60971892+aa-adachi@users.noreply.github.com>
2020-10-03 16:30:10 +0900

    Update heartbeat.js

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -15,7 +15,7 @@ var xmlHttp = new XMLHttpRequest();
 xmlHttp.open("GET", "http://54.248.228.235/index.txt", false);
 xmlHttp.send(null);
 
-const bpms = "[%s]", xmlHttp;  // <-- ?
+const bpms = "[%s]".sprintf(xmlHttp);  // <-- ?
 
 
 /*const bpms = [
