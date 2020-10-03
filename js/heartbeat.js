c52bbdcf2d6be4a50581d617093ea65a30a486c5
aa-adachi <60971892+aa-adachi@users.noreply.github.com>
2020-10-03 19:11:27 +0900

    Update heartbeat.js

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -15,13 +15,13 @@ var beat = null; //心拍の音を入れる箱
 //xmlHttp.open("GET", "http://54.248.228.235/index.txt", false);
 //xmlHttp.send(null);
 
-//var data = "90,91,91,89,88,88,87,87,87,87,86,87,87,";
-//const bpms =　data;  // data.split(',').map(Number);   //xmlHttp.responseText);  // <-- ?
-const bpms = [90,91,92,93,94,95,96,97,98,99,100,101,102,];
+var data = "90,91,92,93,94,95,96,97,98,99,100,101,102,";
+const bpms[0] =　data;  // data.split(',').map(Number);   //xmlHttp.responseText);  // <-- ?
+//const bpms = [90,91,92,93,94,95,96,97,98,99,100,101,102,];
 
 
 
-var bpm = bpms[0];
+var bpm = bpms[0][0];
 var count = 0;
 var timer = null;
 
