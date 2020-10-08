cd1aaa30404beb7fca68b31a34b36a8936af5992
aa-adachi <60971892+aa-adachi@users.noreply.github.com>
2020-10-08 09:17:53 +0900

    Update heartbeat.js

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -35,7 +35,7 @@ xhr.onload = function(e) {
 }
 xhr.send(null);
 
-const bpms = data;//(data.response.slice(0, -1) ).split(',').map( str => parseInt(str, 10) );  // 配列に格納 ( 文字列 --> 数値 )
+const bpms = (data.response.slice(0, -1) ).split(',').map( str => parseInt(str, 10) );  // 配列に格納 ( 文字列 --> 数値 )
 //const bpms = [97,98,99,100,101,102,];
 /* ---------------------- */
 
