09aa2fddb998a59405a2174e9df06e8e1a553248
aa-adachi <60971892+aa-adachi@users.noreply.github.com>
2021-04-15 14:39:33 +0900

    Update heartbeat.js

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -16,7 +16,7 @@ var data = "64,65,";
 var xhr = new XMLHttpRequest(); 
 xhr.withCredentials = true;
 
-xhr.open('GET', 'http://192.168.73.170/test/test.txt', false);
+xhr.open('GET', 'http://(domain)/test/test.txt', false);
 
 xhr.onload = function(e) {
   if (xhr.readyState === 4) {
