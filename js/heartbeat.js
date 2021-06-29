e9ba5c61718eb2cc96208788573f41e70d359d28
aa-adachi <60971892+aa-adachi@users.noreply.github.com>
2021-06-29 14:58:49 +0900

    Update heartbeat.js

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -109,19 +109,21 @@ function play() {
     document.body.style.backgroundColor = '#ffa6a6';
     currentNote = 0;
     document.getElementById("bpm").innerHTML = bpm;
+    document.getElementById("area1").innerText = "stop";
     startTimer();
     nextNoteTime = context.currentTime;
     timerWorker.postMessage("start");
     return "stop";
-    document.getElementById("area1").innerText = "stop";
+    
   } 
   else {
     stopTimer();
     document.body.style.backgroundColor = '#ffffff';
     document.getElementById("bpm").innerHTML = 0;
+    document.getElementById("area1").innerText = "play";
     timerWorker.postMessage("stop");
     return "play";
-    document.getElementById("area1").innerText = "play";
+    
     
   }
 }
