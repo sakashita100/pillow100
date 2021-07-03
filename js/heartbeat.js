92f66188e64100e2eb88433d317079a47c6968dc
aa-adachi <60971892+aa-adachi@users.noreply.github.com>
2021-07-04 00:42:51 +0900

    Update heartbeat.js

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -127,7 +127,7 @@ function play() {
     document.getElementById("bpm").innerHTML = 0;
     document.getElementById("area1").innerHTML = '<p><font size="10">play</font></p>';
     document.getElementById("target").style.backgroundColor = '#ffffff';
-    document.getElementById("heart").innerHTML = '<input type="image" id="target" src="heart.png" class="play" alt="button" width="200" height="200" onclick="play();" >';
+    document.getElementById("heart").innerHTML = '<input type="image" id="target" src="heart.png" class="play" alt="button" onclick="play();" >';
     timerWorker.postMessage("stop");
     return "play";
     
