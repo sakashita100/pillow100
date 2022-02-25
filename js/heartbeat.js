f436a227c6ec75c8bd3f33764edbedaa7af0c410
aa-adachi <60971892+aa-adachi@users.noreply.github.com>
2022-02-25 14:04:48 +0900

    Update heartbeat.js

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -23,9 +23,9 @@ xhr.onload = function(e) {
   if (xhr.readyState === 4) {
     data = "80,81,";
     if (xhr.status === 200) {
-      //data = xhr.responseText;
-      data = "90,91,";
-      //console.log(data)
+      data = xhr.responseText;
+      //data = "90,91,";
+      console.log(data)
     }
   }
 }
