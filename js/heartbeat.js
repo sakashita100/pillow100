c3841b5a86a84315bdfbabb4ee4d705611d868a7
aa-adachi <60971892+aa-adachi@users.noreply.github.com>
2022-02-25 15:04:44 +0900

    Update heartbeat.js

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -16,7 +16,6 @@ var beat = null; //心拍の音を入れる箱
 var data = "70,71,";
 var xhr = new XMLHttpRequest(); 
 xhr.withCredentials = true;
-
 xhr.open('GET', 'https://heartbeat-get.tk/', true);
 
 xhr.onload = function(e) {
